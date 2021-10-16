e0b6cd6d9e869ee8c50a5e7f89f9f5fd6dd4c39b
ashleyn <ashleynavo@googlemail.com>
2021-10-16 17:17:44 +0100

    fizzbuzz test and function

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -2,24 +2,24 @@ const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
   it("returns Fizz when passed a multiple of 3", () => {
-    expect(12 % 3).toEqual(0);
-    expect(27 % 3).toEqual(0);
-    expect(18 % 3).toEqual(0)
+    expect(fizzBuzz(9)).toEqual('Fizz');
+    expect(fizzBuzz(27)).toEqual('Fizz');
+    expect(fizzBuzz(18)).toEqual('Fizz')
   });
 
   it("returns Buzz when passed a multiple of 5", () => {
-    expect(25 % 5).toEqual(0);
-    expect(50 % 5).toEqual(0);
-    expect(20 % 5).toEqual(0);
+    expect(fizzBuzz(50)).toEqual('Buzz');
+    expect(fizzBuzz(25)).toEqual('Buzz');
+    expect(fizzBuzz(20)).toEqual('Buzz');
   });
 
   it("returns FizzBuzz when passed a multiple 3 and 5", () => {
-    expect(15 % 5 && 15 % 3).toEqual(0);
-    expect(30 % 5 && 15 % 3).toEqual(0)
+    expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    expect(fizzBuzz(30)).toEqual('FizzBuzz')
   });
 
   it("returns the number when it isn't a multiple of 3 or 5", () => {
-    expect(4 % 5 ).toEqual(4);
-    expect(2 % 3).toEqual(2);
+    expect(fizzBuzz(4)).toEqual(4);
+    expect(fizzBuzz(2)).toEqual(2);
   });
 });
