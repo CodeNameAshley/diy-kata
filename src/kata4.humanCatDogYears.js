9a391f1e2181ed5167d259284dbda602aea86bde
ashleyn <ashleynavo@googlemail.com>
2021-10-16 19:10:33 +0100

    humanCatDogYears test and function

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,12 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    let catYear3Plus = ((number  - 2) * 4) ;
+    let year1and2 = 15 + 9;
+    let catYears = year1and2 + catYear3Plus ;
+
+    let dogYear3Plus = ((number - 2) * 5);
+    let dogYears = year1and2 + dogYear3Plus;
+
+    return [number, catYears, dogYears]
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,7 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    it("returns an array of human year, cat year, and dog year", () => {
+      expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    });
+  });
