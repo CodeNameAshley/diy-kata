3d03d658e6be167c0a5821fd975294d5752ad40b
ashleyn <ashleynavo@googlemail.com>
2021-10-16 18:29:58 +0100

    numberToReversedDigits test and function

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,6 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    return String(number).split("").reverse().map(Number);
+  //return Array.from(String(number), Number); - DOES NOT REVERSE ARRAY
+};
 
 module.exports = numberToReversedDigits;
