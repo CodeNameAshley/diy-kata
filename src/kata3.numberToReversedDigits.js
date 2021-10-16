3c092582560677906eb026b46382b12b99f56a5d
ashleyn <ashleynavo@googlemail.com>
2021-10-16 18:32:23 +0100

    numberToReversedDigits test and function

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,4 +1,5 @@
 const numberToReversedDigits = number => {
+    // Turns number parameter into a String, then splits it at "", reverses the sequence, with map it turns number(currently a string) into an array of Numbers
     return String(number).split("").reverse().map(Number);
   //return Array.from(String(number), Number); - DOES NOT REVERSE ARRAY
 };
