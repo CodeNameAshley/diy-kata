266ad70dd3be6cd440e8b811506763e3f934384a
ashleyn <ashleynavo@googlemail.com>
2021-10-16 18:00:15 +0100

    booleanToWord test and function

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns Yes when booleanToWord is true", () => {
+    expect(booleanToWord(true)).toEqual("Yes");
+  });
+
+  it("returns NO when booleanToWord is false", () => {
+    expect(booleanToWord(false)).toEqual("No");
+  });
 });
