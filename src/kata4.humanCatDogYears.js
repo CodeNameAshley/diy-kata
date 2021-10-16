c99409b810cd761a5414cade9344a91a6d270970
ashleyn <ashleynavo@googlemail.com>
2021-10-16 19:17:56 +0100

    humanCatDogYears test and function

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,12 +1,17 @@
 const humanCatDogYears = number => {
-    let catYear3Plus = ((number  - 2) * 4) ;
-    let year1and2 = 15 + 9;
-    let catYears = year1and2 + catYear3Plus ;
+  // this variable is the same for both cat and doge
+  const year1and2 = 15 + 9;
 
-    let dogYear3Plus = ((number - 2) * 5);
-    let dogYears = year1and2 + dogYear3Plus;
+  // variables for  cat and doge  past year 3
+  const catYear3Plus = (number - 2) * 4;
+  const dogYear3Plus = (number - 2) * 5;
 
-    return [number, catYears, dogYears]
+  // variables for calculating the sum of cat and dog years with year 1 + year 2 + year  3 plus
+  const catYears = year1and2 + catYear3Plus;
+  const dogYears = year1and2 + dogYear3Plus;
+
+  // returns number(parameter), and variables catYears and dogYears in an array
+  return [number, catYears, dogYears];
 };
 
 module.exports = humanCatDogYears;
