ad3eb35946db668b738f6ca1db3942e600a2ad3f
ashleyn <ashleynavo@googlemail.com>
2021-10-20 17:36:34 +0100

    further answers

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,5 +1,12 @@
+// const getEmployerRole = (employeeName, employees) => {
+//   return employees.find(employee => employee.name === employeeName);
+// };
+
 const getEmployerRole = (employeeName, employees) => {
-  return employees.find(employee => employee.name === employeeName);
+  let employeeProfile = employees.find(person => {
+    return person.name === employeeName;
+  });
+  return employeeProfile.role;
 };
 
 module.exports = getEmployerRole;
